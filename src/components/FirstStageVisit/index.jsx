29b80cd49fcc10425d66e62bb0daac87e13fde58
nader <esmailjafarpour29@gmail.com>
2023-01-15 11:30:39 +0330

    Add fonts to the project

--- src/components/FirstStageVisit/index.jsx
+++ src/components/FirstStageVisit/index.jsx
@@ -1,21 +1,22 @@
 import './style.css';
 import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 const FirstStageVisit =  () => {
     return (
-            <Box
-                className="visit-Box"
-              sx={{
-               
+            <Box className="visit-Box">
 
-              }}
-            >
                 <Box className="Initial-visit">
-
+                    <Typography variant="h6" component="h2">
+                        نوبت اولیه
+                    </Typography>
                 </Box>
                 <Box className="Next-visit">
-
+                    <Typography variant="h6" component="h2">
+                        نوبت بعدی
+                    </Typography>
                 </Box>
+                
             </Box>
     );
 }
